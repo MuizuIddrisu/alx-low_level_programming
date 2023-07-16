0b66d43d189783b756cc89d7f0c47325211a4ad5
MuizuIddrisu <muizuiddrisu.edu@gmail.com>
2023-07-16 02:23:48 -0700

    Update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /**
-*main - entry point
+*main - the entry point
 *
 *return: 0 success
 */
-
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
